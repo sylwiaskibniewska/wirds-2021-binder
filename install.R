cd58308eb35d7cbbab16941c877ce69e69e463c4
sylwiaskibniewska <77264819+sylwiaskibniewska@users.noreply.github.com>
2021-01-11 08:40:17 +0100

    Create install.R

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages("tidyverse") ## przetwarzanie
+install.packages("data.table") ## do ramek danych
+install.packages("readxl") ## do wczytywania plików excela
+install.packages("sf") ## wczytanie plików z mapami
